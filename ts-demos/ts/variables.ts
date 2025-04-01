74a3bde4d34762287d2445faf0a69247feb2f993
christian-meyer <christianmeyerxxx@gmail.com>
2025-04-01 09:54:23 -0400

    init commit

---
+++ ts-demos/ts/variables.ts
@@ -0,0 +1,27 @@
+let a: number = 5;
+let b: number = 3.14;
+let c: number = a+b;
+console.log("c", c);
+
+let firstName: string = "Bob";
+let lastName: string = "Marley";
+let fullName:string = firstName+lastName;
+console.log(fullName);
+
+let isValid: boolean = false;
+console.log(a === 5);
+
+let movie: object = {
+    "id":1,
+    "title": "cool movie",
+    "year": 2026,
+    "rating": "PG",
+    "director": "Some Director"
+}
+
+console.log("movie",movie)
+
+let numbers: number[] = [0,1,2,3,4,5];
+numbers.push(6)
+console.log("numbers", numbers);
+numbers.forEach(e=> console.log(e))
